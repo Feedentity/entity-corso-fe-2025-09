d11113397544d29e250c251077ace3f122eaa973
FrancescoB <francesco.bottiglia10@gmail.com>
2025-09-19 15:36:29 +0200

    Implementazione finita delle zone

---
+++ fe/app/Product/Zona/zona.js
@@ -0,0 +1,31 @@
+var app = angular.module('app',[]);
+
+app.directive("zoneCimitero", function() {
+    return {
+        scope:{
+            zona:"<z"
+        },
+        restrict : "A",
+        template : `
+            <td>{{zona.nome}}</td>
+            <td>{{zona.capienza}}</td>
+            <td>{{zona.capienza - zona.listaTombe.length}}</td>
+            <td>{{zona.descrizione}}</td>
+            <td>{{zona.sovrapprezzo}}%</td>
+        `
+    }
+})
+
+app.controller("zonaCtrl", function($scope, zoneService) {
+    $scope.zone = [];
+    zoneService.getZone().then (function(response) {
+    $scope.zone = response.data.zone;
+    }
+)
+})
+
+app.service("zoneService", function($http){
+    this.getZone = function() {
+        return $http.get("/be/db.json");
+    }
+})
