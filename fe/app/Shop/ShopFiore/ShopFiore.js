843fd9aa0b435b41493e636cd906c7580b5954be
DrAndre46 <dr.andre46@gmail.com>
2025-09-19 13:06:26 +0200

    Implementazione Shop Fiore

---
+++ fe/app/Shop/ShopFiore/ShopFiore.js
@@ -0,0 +1,20 @@
+var app = angular.module("app", []);
+
+// Controller che gestisce la pagina ShopFiore
+app.controller("ShopFioreCtrl", function ($scope, ShopFioreService) {
+  $scope.fiori = [];
+
+  ShopFioreService.getDati().then(function (response) {
+    $scope.fiori = response.data.tipoFiori;
+  });
+
+  $scope.AggiungiAlCarrello = function () {
+    console.log("Aggiungi al carrello");
+  };
+});
+
+// Service che gestisce le operazioni di accesso al database
+app.service("ShopFioreService", function ($http) {
+  this.getDati = function () {
+    return $http.get("/be/db.json")
+    }});
