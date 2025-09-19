78d776bee552b741d084a89cde18972e416412b5
Adriano Koci <kociadrianok918@gmail.com>
2025-09-19 12:59:02 +0200

    implementazione versione fiori

---
+++ fe/app/Product/VersioneFiore/FioreVersionControl.js
@@ -0,0 +1,9 @@
+var app = angular.module('app', [])
+
+app.controller("listaFioriCtrl",function($scope,serviceListaFiori) {//creo il controller principale per la pagina
+
+    $scope.listaFiori = [];//creo una lista 
+    $scope.listaFiori = serviceListaFiori.oggetto;//all'interno della lista inserisco i dati dell back end mandati dall service
+
+});
+
---
+++ fe/app/Product/VersioneFiore/ServiceVersionFiori.js
@@ -0,0 +1,12 @@
+app.service("serviceListaFiori",function($http) {/*sto creando un servizio il qui scopo e quello di fare una chiamata al be per prendere la lista dei fiori */
+    function funzioneLista() {
+        var oggetto = {}
+        $http.get('http://localhost:3000/tipoFiori').then(function(response){/* effettuo la chiamata al be*/
+        var dati_json = response.data;/* creo una variabile e all interno inserisco i dati di risposta dall'be  */
+        oggetto.dati = dati_json;/* inserisco i dati dentro all oggetto */
+    });
+        return oggetto;
+    }
+    let oggettoLista = {oggetto : funzioneLista()}//creo un altro oggetto dove all interno metto dentro la funzione
+    return oggettoLista;
+});
