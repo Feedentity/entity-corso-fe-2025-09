0b129673e397f9b9528324371de9ca375883c041
Nico <nicovasta2806@gmail.com>
2025-09-19 13:10:08 +0200

    Implementazione lista versione tombe

---
+++ fe/app/Product/VersioneTomba/TombaVersion.js
@@ -0,0 +1,15 @@
+var app=angular.module('app', [])
+app.controller('tombaVersionCtrl', function($scope, tombaVersionService){
+    $scope.versioneTombe=[];
+
+    tombaVersionService.getTombe().then(function(response){
+        $scope.versioneTombe = response.data.versioneTombe;
+    })
+})
+
+app.service('tombaVersionService', function($http){
+    this.getTombe = function(){
+    return $http.get("/be/db.json")
+    }
+})
+
