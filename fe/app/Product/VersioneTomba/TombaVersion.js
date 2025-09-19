caf2beab1c2c41a498b4ffcadcd0bfedf71956dd
Nico <nicovasta2806@gmail.com>
2025-09-19 17:39:25 +0200

    aggiunta form creazione versioni tombe e modifica relativo controller

--- fe/app/Product/VersioneTomba/TombaVersion.js
+++ fe/app/Product/VersioneTomba/TombaVersion.js
@@ -1,14 +1,31 @@
 app.controller('tombaVersionCtrl', function($scope, tombaVersionService){
     $scope.versioneTombe=[];
+    $scope.versioneTombe= tombaVersionService.getTombe();
 
-    tombaVersionService.getTombe().then(function(response){
-        $scope.versioneTombe = response.data.versioneTombe;
-    })
-})
+    $scope.newTomba ={}
 
-app.service('tombaVersionService', function($http){
-    this.getTombe = function(){
-    return $http.get("/be/db.json")
+    $scope.submitTomba = function(){
+        tombaVersionService.createTomba($scope.newTomba).then(function(response){
+            $scope.versioneTombe.push(response.data)
+        });
+
+        $scope.newTomba="";
     }
 })
+
+app.service('tombaVersionService', function($resource){
+   var resources = $resource('http://localhost:4000/versioneTombe/:id', {id: '@id'},
+    {
+        update: {method: 'PUT'}
+    })
+
+    this.getTombe = function(){
+        return resources.query();
+    }
+
+    this.createTomba = function(tomba){
+        return resources.save(tomba).$promise
+    }
+})
+
 
