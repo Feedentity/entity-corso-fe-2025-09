c07964b51d269d0b2e361b8c93f4835e5ea1fe09
DrAndre46 <dr.andre46@gmail.com>
2025-09-19 16:36:49 +0200

    Aggiunta del css e sistemazione file vari

--- fe/app/Product/VersioneFiore/ServiceVersionFiori.js
+++ fe/app/Product/VersioneFiore/ServiceVersionFiori.js
@@ -1,7 +1,7 @@
 app.service("serviceListaFiori",function($http) {/*sto creando un servizio il qui scopo e quello di fare una chiamata al be per prendere la lista dei fiori */
     function funzioneLista() {
         var oggetto = {}
-        $http.get('http://localhost:3000/tipoFiori').then(function(response){/* effettuo la chiamata al be*/
+        $http.get('http://localhost:4000/tipoFiori').then(function(response){/* effettuo la chiamata al be*/
         var dati_json = response.data;/* creo una variabile e all interno inserisco i dati di risposta dall'be  */
         oggetto.dati = dati_json;/* inserisco i dati dentro all oggetto */
     });
