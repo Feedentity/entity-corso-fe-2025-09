12dd73e7cef622266857dfc46f4e2970bc164075
Adriano Koci <kociadrianok918@gmail.com>
2025-09-22 12:49:50 +0200

    implentazione resource su FioreVersion

--- fe/app/Product/VersioneFiore/FioreVersionControl.js
+++ fe/app/Product/VersioneFiore/FioreVersionControl.js
@@ -1,7 +1,37 @@
-app.controller("listaFioriCtrl",function($scope,serviceListaFiori) {//creo il controller principale per la pagina
 
-    $scope.listaFiori = [];//creo una lista 
-    $scope.listaFiori = serviceListaFiori.oggetto;//all'interno della lista inserisco i dati dell back end mandati dall service
+app.controller("listaFioriCtrl",function($scope,serviceListaFiori) {//creo il controller principale per la pagina
+    $scope.datiFiori = [];
+    $scope.datiFiori = serviceListaFiori.getFiori();
+
+    $scope.newFiore = {}
+    $scope.idFiori = {}
+
+    $scope.submitFiore = function(){
+        serviceListaFiori.createFiori($scope.newFiore).then(function(response){
+            $scope.datiFiori.push(response.data)
+        })
+    }
+
+$scope.cancella = function(fiore){
+    var idDaCancellare = $scope.datiFiori.indexOf(fiore);
+
+    serviceListaFiori.deleteFiori(fiore).then(function(){
+        $scope.datiFiori.splice(idDaCancellare, 1)
+    })
+   /* if (!idDaCancellare) {
+        console.warn('ID non fornito. Inserisci un ID valido.');
+        return;
+    }
+    serviceListaFiori.deleteFiori({ id: idDaCancellare }).then(function(response){
+        const index = $scope.datiFiori.findIndex(fiore => fiore.id == idDaCancellare);
+        if (index !== -1) {
+            $scope.datiFiori.splice(index, 1);
+            console.log('Cancellato correttamente dalla view.');
+        } 
+    }).catch(function(error){
+        console.error('Errore durante la cancellazione del fiore:', error);
+    }); */
+};  
 
 });
 
--- fe/app/Product/VersioneFiore/ServiceVersionFiori.js
+++ fe/app/Product/VersioneFiore/ServiceVersionFiori.js
@@ -1,12 +1,15 @@
-app.service("serviceListaFiori",function($http) {/*sto creando un servizio il qui scopo e quello di fare una chiamata al be per prendere la lista dei fiori */
-    function funzioneLista() {
-        var oggetto = {}
-        $http.get('http://localhost:4000/tipoFiori').then(function(response){/* effettuo la chiamata al be*/
-        var dati_json = response.data;/* creo una variabile e all interno inserisco i dati di risposta dall'be  */
-        oggetto.dati = dati_json;/* inserisco i dati dentro all oggetto */
-    });
-        return oggetto;
-    }
-    let oggettoLista = {oggetto : funzioneLista()}//creo un altro oggetto dove all interno metto dentro la funzione
-    return oggettoLista;
-});
+app.service("serviceListaFiori",['$resource',function($resource) {
+        var resources = $resource('http://localhost:4000/tipoFiori/:id', { id: '@id' })
+
+        this.getFiori = function(){
+                return resources.query();
+        }
+
+        this.createFiori = function(fiore){
+                return resources.save(fiore).$promise
+        }
+
+        this.deleteFiori = function(fiore){
+                return resources.delete(fiore).$promise
+        }
+}]);
