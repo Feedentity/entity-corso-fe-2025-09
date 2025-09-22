e350fb359282e5d1d7326b71448edc68ab154f9d
DrAndre46 <dr.andre46@gmail.com>
2025-09-22 12:50:32 +0200

    css login/registrazione e implementazione shopFiore

--- fe/app/Shop/ShopFiore/ShopFiore.js
+++ fe/app/Shop/ShopFiore/ShopFiore.js
@@ -1,18 +1,66 @@
-// Controller che gestisce la pagina ShopFiore
+// Questo controller gestisce tutto quello che succede nella pagina del negozio di fiori
 app.controller("ShopFioreCtrl", function ($scope, ShopFioreService) {
+    
+  // Inizializziamo le variabili
   $scope.fiori = [];
+  $scope.carrello = [];
+  $scope.mostraCarrello = false;
 
-  ShopFioreService.getDati().then(function (response) {
-    $scope.fiori = response.data.tipoFiori;
+  ShopFioreService.getFiori().then(function(response) {
+    $scope.fiori = response; 
   });
 
-  $scope.AggiungiAlCarrello = function () {
-    console.log("Aggiungi al carrello");
+  // Funzione per aggiungere un fiore al carrello
+  $scope.AggiungiAlCarrello = function (fiore) {
+    
+    var fioreEsistente = $scope.carrello.find(function (item) {
+      return item.nome === fiore.nome;
+    });
+
+    if (fioreEsistente) {
+      fioreEsistente.quantita = fioreEsistente.quantita + 1;
+    } else {
+      $scope.carrello.push({
+        nome: fiore.nome,
+        descrizione: fiore.descrizione,
+        prezzo: fiore.prezzo,
+        quantita: 1,
+      });
+    }
+  };
+
+  // Funzione per mostrare/nascondere il carrello
+  $scope.ToggleCarrello = function () {
+    $scope.mostraCarrello = !$scope.mostraCarrello;
+  };
+
+  // Funzione per rimuovere un fiore dal carrello
+  $scope.RimuoviDalCarrello = function (index) {
+    $scope.carrello.splice(index, 1);
+  };
+
+  // Funzione per calcolare il totale del carrello
+  $scope.CalcolaTotale = function () {
+    var totale = 0;
+    for (var i = 0; i < $scope.carrello.length; i++) {
+      totale = totale + ($scope.carrello[i].prezzo * $scope.carrello[i].quantita);
+    }
+    
+    return totale;
   };
 });
 
-// Service che gestisce le operazioni di accesso al database
-app.service("ShopFioreService", function ($http) {
-  this.getDati = function () {
-    return $http.get("/be/db.json")
-    }});
+app.service("ShopFioreService", function ($resource) {
+  var resources = $resource(
+    "http://localhost:4000/tipoFiori/:id",
+    { id: "@id" },
+    {
+      update: { method: "PUT" },
+    }
+  );
+  
+  // Funzione per prendere TUTTI i fiori dal database
+  this.getFiori = function () {
+    return resources.query().$promise;
+  };
+});
