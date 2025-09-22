f5a44232ca7b2deac91d0527d71e98baaea38939
FrancescoB <francesco.bottiglia10@gmail.com>
2025-09-22 17:44:48 +0200

    Completate funzioni zone tombe

--- fe/app/Product/Zona/zona.js
+++ fe/app/Product/Zona/zona.js
@@ -1,7 +1,8 @@
 app.directive("zoneCimitero", function() {
     return {
         scope:{
-            zona:"<z"
+            zona:"<z",
+            actions:"<a" 
         },
         restrict : "A",
         template : `
@@ -10,20 +11,73 @@ app.directive("zoneCimitero", function() {
             <td>{{zona.capienza - zona.listaTombe.length}}</td>
             <td>{{zona.descrizione}}</td>
             <td>{{zona.sovrapprezzo}}%</td>
+            <td>
+                <button type='submit' ng-click='!actions.removeZona(zona)'>Elimina</button>
+                <button type='submit' ng-if='!actions.edit' ng-click='!actions.editZona()'>Modifica</button>
+                <button type='submit' ng-if='actions.edit' ng-click='!actions.saveZona(zona)'>Salva</button>
+            </td>
         `
     }
-})
+});
 
 app.controller("zonaCtrl", function($scope, zoneService) {
-    $scope.zone = [];
-    zoneService.getZone().then (function(response) {
-    $scope.zone = response.data.zone;
-    }
-)
-})
+    $scope.zoneTombe = [];
 
-app.service("zoneService", function($http){
-    this.getZone = function() {
-        return $http.get("/be/db.json");
+    $scope.edit=false;
+
+    $scope.zoneTombe = zoneService.getZone();
+    
+    $scope.newZona ={}
+
+    $scope.azioni = $scope
+
+    $scope.submitZona = function() {
+        zoneService.createZona($scope.newZona).then(function(response) {
+            $scope.zoneTombe.push(response.data)
+        });
+
+        $scope.newTomba={};
     }
-})
+
+    $scope.removeZona = function(zona) {
+        let i = $scope.zoneTombe.indexOf(zona);
+        zoneService.deleteZona(zona).then(function(){
+            $scope.zoneTombe.splice(i, 1);
+        })
+    }
+
+    $scope.editZona = function() {
+        $scope.edit = true;  
+    }
+
+    $scope.saveZona = function(zona) {
+        let i = $scope.zoneTombe.indexOf(zona);
+        zoneService.updateZona(zona)
+        $scope.zoneTombe[i] = zona;
+        $scope.edit = false;
+    }
+});
+
+app.service("zoneService", function($resource){
+    var resources = $resource('http://localhost:4000/zone/:id', {id: '@id'}, 
+    {
+        update: {method: 'PUT'}
+    }
+   )
+
+    this.getZone = function(){
+        return resources.query();
+    }
+
+    this.createZona = function(zona) {
+        return resources.save(zona).$promise
+    }
+
+    this.deleteZona = function(zona) {
+        return resources.delete(zona).$promise;
+    }
+
+    this.updateZona = function(zona) {
+        return resources.update({id: zona.id}, zona).$promise
+    }
+});
