01c688a84e33f4cc57dee4829eca51c19e4feecc
Nico <nicovasta2806@gmail.com>
2025-09-22 12:42:13 +0200

    Implementazione funzioni modifica e elimina versioni tombe

--- fe/app/Product/VersioneTomba/TombaVersion.js
+++ fe/app/Product/VersioneTomba/TombaVersion.js
@@ -1,7 +1,10 @@
 app.controller('tombaVersionCtrl', function($scope, tombaVersionService){
     $scope.versioneTombe=[];
-    $scope.versioneTombe= tombaVersionService.getTombe();
 
+    $scope.edit=false;
+
+    $scope.versioneTombe= tombaVersionService.getTombe();
+    
     $scope.newTomba ={}
 
     $scope.submitTomba = function(){
@@ -9,15 +12,34 @@ app.controller('tombaVersionCtrl', function($scope, tombaVersionService){
             $scope.versioneTombe.push(response.data)
         });
 
-        $scope.newTomba="";
+        $scope.newTomba={};
+    }
+
+    $scope.removeTomba = function(tomba){
+        let i = $scope.versioneTombe.indexOf(tomba);
+        tombaVersionService.deleteTomba(tomba).then(function(){
+            $scope.versioneTombe.splice(i, 1);
+        })
+    }
+
+    $scope.editTomba = function(){
+        $scope.edit = true;  
+    }
+
+    $scope.saveTomba = function(tomba){
+        let i = $scope.versioneTombe.indexOf(tomba);
+        tombaVersionService.updateTomba(tomba)
+        $scope.versioneTombe[i] = tomba;
+        $scope.edit = false;
     }
 })
 
 app.service('tombaVersionService', function($resource){
-   var resources = $resource('http://localhost:4000/versioneTombe/:id', {id: '@id'},
+   var resources = $resource('http://localhost:4000/versioneTombe/:id', {id: '@id'}, 
     {
         update: {method: 'PUT'}
-    })
+    }
+   )
 
     this.getTombe = function(){
         return resources.query();
@@ -26,6 +48,16 @@ app.service('tombaVersionService', function($resource){
     this.createTomba = function(tomba){
         return resources.save(tomba).$promise
     }
+
+    this.deleteTomba = function(tomba){
+        return resources.delete(tomba).$promise;
+    }
+
+    this.updateTomba = function(tomba){
+        return resources.update({id: tomba.id}, tomba).$promise
+    }
 })
+
+
 
 
